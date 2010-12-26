18ce8503d721fd1af65ac00de3a4d94c6c79276a
Emmanuel Bourgerie <manu.dwarf@gmail.com>
2010-12-26 13:28:47 +0100

    Page d'accueil

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Plumes du web</title>
 		<link href='http://fonts.googleapis.com/css?family=Cantarell' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Josefin+Sans:regular,bold' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Bentham' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<link rel="stylesheet" type="text/css" href="css/footer.css" />
 		<link rel="stylesheet" type="text/css" href="css/header.css" />
 		<link rel="stylesheet" type="text/css" href="css/newsfeed.css" />
 		<? include_once 'fragments/flattr.php'; ?>
@@ -31,25 +32,14 @@
 						<li><b>Bande dessinée</b></li>
 						<li><b>Manga</b></li>
 					</ul>
-				</div>
-
-				<div id="infos_block">
-					<div id="last_blog">
-						<h1>Blog</h1>
-						<h2>Ouverture du site</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse metus orci, pharetra in congue ut, luctus vitae nisl. Vestibulum placerat elit sed ante viverra a aliquam quam rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce in mi arcu. Quisque sit amet massa tristique nisi imperdiet egestas sit amet id libero. Curabitur congue, diam eu dictum laoreet, enim purus sodales mauris, eget ultricies justo lacus ac turpis. Donec pharetra massa et nibh vulputate et posuere justo sollicitudin. Donec commodo nisi sit amet sapien sodales quis suscipit quam auctor. Curabitur aliquam placerat erat, vel sodales nisi congue id. Maecenas vitae justo sed ipsum lacinia lacinia. Ut bibendum adipiscing massa et condimentum.</p>
-					</div>
-					<div id="infos_pdw">
-						<h1>Qu'est ce que Plumes du Web ?</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse metus orci, pharetra in congue ut, luctus vitae nisl. Vestibulum placerat elit sed ante viverra a aliquam quam rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce in mi arcu. Quisque sit amet massa tristique nisi imperdiet egestas sit amet id libero. Curabitur congue, diam eu dictum laoreet, enim purus sodales mauris, eget ultricies justo lacus ac turpis. Donec pharetra massa et nibh vulputate et posuere justo sollicitudin. Donec commodo nisi sit amet sapien sodales quis suscipit quam auctor. Curabitur aliquam placerat erat, vel sodales nisi congue id. Maecenas vitae justo sed ipsum lacinia lacinia. Ut bibendum adipiscing massa et condimentum.</p>
-						<p>Praesent tincidunt urna a augue rhoncus quis aliquam justo aliquet. Proin rhoncus tincidunt nulla, vel varius ligula adipiscing sit amet. Curabitur quis arcu at velit lobortis convallis. In ornare erat quis augue euismod et sollicitudin tortor sodales. Nullam pulvinar rhoncus sollicitudin. Duis dignissim, nunc eu ultricies facilisis, neque lectus posuere eros, at venenatis arcu nulla eu mauris. Nulla aliquet adipiscing elit, eget iaculis mauris tincidunt at. Aliquam erat volutpat. Integer vitae odio eget felis cursus porttitor. Vestibulum vitae dolor convallis lorem molestie condimentum eu quis urna. Sed nec mi augue. Suspendisse lacinia volutpat sollicitudin. Curabitur rutrum placerat vulputate. Sed vitae quam vitae nibh convallis rhoncus. Mauris accumsan, justo in lacinia bibendum, risus turpis bibendum mauris, sit amet volutpat magna tellus quis lacus. Nunc ut mattis magna. Aliquam erat volutpat. Nulla libero lectus, ultrices et laoreet non, scelerisque vitae metus. Aliquam fringilla porttitor viverra.</p>
+					<div id="twitter_aggreg">
+						Ici, aggrégation Twitter.
 					</div>
 				</div>
-			</div>
-
-			<div id="footer">
 
 			</div>
+
+			<? include 'fragments/footer.php';?>
 		</div>
 	</body>
 </html>
