5492a7b5c59158fd2f3b94cebaac9f40e36a77da
Emmanuel BOURGERIE <manu.dwarf@gmail.com>
2010-12-26 14:46:37 +0100

    Présentation pied

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,12 +1,14 @@
 <div id="footer">
 	<div id="last_blog">
 		<h1>Blog</h1>
-		<h2>Ouverture du site</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse metus orci, pharetra in congue ut, luctus vitae nisl. Vestibulum placerat elit sed ante viverra a aliquam quam rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce in mi arcu. Quisque sit amet massa tristique nisi imperdiet egestas sit amet id libero. Curabitur congue, diam eu dictum laoreet, enim purus sodales mauris, eget ultricies justo lacus ac turpis. Donec pharetra massa et nibh vulputate et posuere justo sollicitudin. Donec commodo nisi sit amet sapien sodales quis suscipit quam auctor. Curabitur aliquam placerat erat, vel sodales nisi congue id. Maecenas vitae justo sed ipsum lacinia lacinia. Ut bibendum adipiscing massa et condimentum.</p>
+		<table width="100%">
+		<tr>
+		<td>Ouverture du site</td><td align="right">26 oct.</td>
+		</tr>
+		</table>
 	</div>
 	<div id="infos_pdw">
 		<h1>Qu'est ce que Plumes du Web ?</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse metus orci, pharetra in congue ut, luctus vitae nisl. Vestibulum placerat elit sed ante viverra a aliquam quam rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce in mi arcu. Quisque sit amet massa tristique nisi imperdiet egestas sit amet id libero. Curabitur congue, diam eu dictum laoreet, enim purus sodales mauris, eget ultricies justo lacus ac turpis. Donec pharetra massa et nibh vulputate et posuere justo sollicitudin. Donec commodo nisi sit amet sapien sodales quis suscipit quam auctor. Curabitur aliquam placerat erat, vel sodales nisi congue id. Maecenas vitae justo sed ipsum lacinia lacinia. Ut bibendum adipiscing massa et condimentum.</p>
-		<p>Praesent tincidunt urna a augue rhoncus quis aliquam justo aliquet. Proin rhoncus tincidunt nulla, vel varius ligula adipiscing sit amet. Curabitur quis arcu at velit lobortis convallis. In ornare erat quis augue euismod et sollicitudin tortor sodales. Nullam pulvinar rhoncus sollicitudin. Duis dignissim, nunc eu ultricies facilisis, neque lectus posuere eros, at venenatis arcu nulla eu mauris. Nulla aliquet adipiscing elit, eget iaculis mauris tincidunt at. Aliquam erat volutpat. Integer vitae odio eget felis cursus porttitor. Vestibulum vitae dolor convallis lorem molestie condimentum eu quis urna. Sed nec mi augue. Suspendisse lacinia volutpat sollicitudin. Curabitur rutrum placerat vulputate. Sed vitae quam vitae nibh convallis rhoncus. Mauris accumsan, justo in lacinia bibendum, risus turpis bibendum mauris, sit amet volutpat magna tellus quis lacus. Nunc ut mattis magna. Aliquam erat volutpat. Nulla libero lectus, ultrices et laoreet non, scelerisque vitae metus. Aliquam fringilla porttitor viverra.</p>
+		<p>Plumes du Web est un site de pré-publication qui a pour but de faire découvrir de jeunes talents du dessin, de la littérature ou de toute forme d'art.</p>
 	</div>
 </div>
