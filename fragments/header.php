02466b6c37192d99107badaef0bbfbc1d577bc85
Emmanuel BOURGERIE <manu.dwarf@gmail.com>
2010-12-26 10:54:59 +0100

    Création dépôt

---
+++ fragments/header.php
@@ -0,0 +1,11 @@
+<div id="header">
+	<ul>
+		<li><a class="FlattrButton" style="display:none;" rev="flattr;button:compact;"
+			   href="http://www.plumesduweb.fr"></a></li>
+		<li><a href="index.php">Accueil</a></li>
+		<li><a href="mag.php">Magazine</a></li>
+		<li><a href="blog/">Blog</a></li>
+		<li><a href="forum/">Forum</a></li>
+	</ul>
+	<h1>Plumes du Web</h1>
+</div>
---
+++ fragments/newsfeed.php
@@ -0,0 +1,4 @@
+<div id="newsfeed">
+	<h1>Ouverture du site</h1>
+	<p>Blah blah blah</p>
+</div>
